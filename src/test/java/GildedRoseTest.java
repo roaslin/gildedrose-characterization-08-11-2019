7b3ee69791c1b8c6519d2a585eea4b70ce362a60
raulcodurance <raul.mordillo@codurance.com>
2019-11-09 18:22:51 +0100

    reset

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -2,6 +2,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/*
+    - All items have a SellIn value which denotes the number of days we have to sell the item
+	- All items have a Quality value which denotes how valuable the item is
+	- At the end of each day our system lowers both values for every item
+
+    - Once the sell by date has passed, Quality degrades twice as fast
+	- The Quality of an item is never negative
+	- "Aged Brie" actually increases in Quality the older it gets
+	- The Quality of an item is never more than 50
+	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+        Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+        Quality drops to 0 after the concert
+
+	This requires an update to our system:
+
+	- "Conjured" items degrade in Quality twice as fast as normal items
+ */
 public class GildedRoseTest {
 
     @Test
@@ -9,7 +27,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
 }
