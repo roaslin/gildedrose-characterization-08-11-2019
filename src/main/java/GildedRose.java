4d51d9445c6a9dd0e3c61bd0ca338bfe8a56b1da
raulcodurance <raul.mordillo@codurance.com>
2019-11-16 18:16:22 +0100

    invert if statements

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -7,54 +7,73 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!"Aged Brie".equals(items[i].name)
-                    && !"Backstage passes to a TAFKAL80ETC concert".equals(items[i].name)) {
-                if (items[i].quality > 0) {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items[i].name)) {
-                        items[i].quality = items[i].quality - 1;
+            Item currentItem = items[i];
+            String itemName = currentItem.name;
+
+            if (isAgedBrie(itemName)
+                    || isBackstagePasses(itemName)) {
+                if (currentItem.quality < 50) {
+                    currentItem.quality = currentItem.quality + 1;
+
+                    if (isBackstagePasses(itemName)) {
+                        if (currentItem.sellIn < 11) {
+                            if (currentItem.quality < 50) {
+                                currentItem.quality = currentItem.quality + 1;
+                            }
+                        }
+
+                        if (currentItem.sellIn < 6) {
+                            if (currentItem.quality < 50) {
+                                currentItem.quality = currentItem.quality + 1;
+                            }
+                        }
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items[i].name)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                if (currentItem.quality > 0) {
+                    if (!isSulfurasHandOfRagnaros(itemName)) {
+                        currentItem.quality = currentItem.quality - 1;
                     }
                 }
             }
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(items[i].name)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            decreaseSellIn(currentItem, itemName);
 
-            if (items[i].sellIn < 0) {
-                if (!"Aged Brie".equals(items[i].name)) {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items[i].name)) {
-                        if (items[i].quality > 0) {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items[i].name)) {
-                                items[i].quality = items[i].quality - 1;
+            if (currentItem.sellIn < 0) {
+                if (isAgedBrie(itemName)) {
+                    if (currentItem.quality < 50) {
+                        currentItem.quality = currentItem.quality + 1;
+                    }
+                } else {
+                    if (!isBackstagePasses(itemName)) {
+                        if (currentItem.quality > 0) {
+                            if (!isSulfurasHandOfRagnaros(itemName)) {
+                                currentItem.quality = currentItem.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        currentItem.quality = currentItem.quality - currentItem.quality;
                     }
                 }
             }
         }
+    }
+
+    private void decreaseSellIn(Item currentItem, String itemName) {
+        if (!isSulfurasHandOfRagnaros(itemName)) {
+            currentItem.sellIn = currentItem.sellIn - 1;
+        }
+    }
+
+    private boolean isSulfurasHandOfRagnaros(String itemName) {
+        return "Sulfuras, Hand of Ragnaros".equals(itemName);
+    }
+
+    private boolean isBackstagePasses(String itemName) {
+        return "Backstage passes to a TAFKAL80ETC concert".equals(itemName);
+    }
+
+    private boolean isAgedBrie(String itemName) {
+        return "Aged Brie".equals(itemName);
     }
 }
